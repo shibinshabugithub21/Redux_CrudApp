0eacbab5bf3871aca7c826018e99233b4de00519
Shibin P Shabu <shibinshabu392@gmail.com>
2024-07-14 20:36:35 +0530

    add the .env to the got gnore

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.js'
+
 dotenv.config();
 
 
 mongoose.connect(process.env.Mongodb).then(()=>{
-    console.log('Conntected to mongoDb')
+    console.log('Connected to mongoDb')
 }).catch((err)=>{
     console.log(err);
 })
@@ -15,3 +17,5 @@ const app=express();
 app.listen(3000,()=>{
     console.log("Server is running on the port 3000");
 });
+
+app.use("/api/user/",userRoutes)
