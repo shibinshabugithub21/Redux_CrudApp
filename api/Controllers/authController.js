0843b83bc64bbf080772e1beff80fe8617667d05
Shibin P Shabu <shibinshabu392@gmail.com>
2024-07-15 00:50:06 +0530

    cookie and signup where settupeed

--- api/Controllers/authController.js
+++ api/Controllers/authController.js
@@ -1,6 +1,10 @@
 import User from '../models/user.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
+
+// signup
 export const signup = async (req, res, next) => {
     console.log('Request Body:', req.body);
     const { username, email, password } = req.body;
@@ -12,5 +16,29 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ message: "Error creating user", error });
+    }
+};
+
+
+// signin
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(401, 'User not found'));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'Invalid password'));
+        }
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:hashPassword,...rest}=validUser._doc;
+        const expiryDate=new Date(Date.now() + 36000000);
+        res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).status(200).json(rest)
+    } catch (error) {
+        next(error); 
     }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express"
 import { signup } from "../Controllers/authController.js"
+import {signin} from '../Controllers/authController.js'
 
 const router=express.Router()
 
 router.post("/signup",signup)
+router.post("/signin",signin)
+
 
 export default router
